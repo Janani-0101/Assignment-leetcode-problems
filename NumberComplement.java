2d45987d587fe7d59b5d43cb28a2124c8cd33802
Janani M <janani.01.jan.07@gmail.com>
2026-03-22 13:57:01 +0530

    Delete NumberComplement.java

--- NumberComplement.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int findComplement(int num) {
-        if(num==0) return 1;
-        int len = Integer.toBinaryString(num).length();
-        int arr = (1<<len)-1;
-        return num^arr;
-    }
-}
